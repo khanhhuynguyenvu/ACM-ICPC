c85e29ce9bac8ce5dd13e472f57dc14087adb128
nguyenvukhanhhuy <khanhhuynguyenvu@gmail.com>
2018-10-07 23:29:48 +0700

    10/07/2018

---
+++ DP/MaxSubset/MaxSubset.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <math.h>
+#include <ctype.h>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+#define mp             make_pair
+#define pb             push_back
+#define fs             first
+#define sc             second
+#define sz(x)          (int)((x).size())
+#define fill(x, y)     memset(x, y, sizeof(x))
+#define prl(x)          cout<<(x)<<" "
+#define Pr(x)          cout<<(x)<<endl
+#define all(x)         (x).begin(), (x).end()
+#define sci(x)         int x;       scanf("%d", &x);
+#define scii(x, y)     int x, y;    scanf("%d %d", &x, &y);
+#define sciii(x, y, z) int x, y, z; scanf("%d %d %d", &x, &y, &z);
+#define TC(x)          sci(x); while(x --)
+#define eprintf(...)   fprintf(stderr, __VA_ARGS__)
+#define debug(x)       { cerr << #x << " = " << x << endl; }
+#define rep(i, x, y)   for (__typeof(x) i = x; i < y; i ++)
+#define repi(i, x, y)  for (__typeof(x) i = x; i >= y; i --)
+#define fore(itr, x)   for (__typeof(x.begin()) itr = x.begin(); itr != x.end(); itr ++)
+#define forei(itr, x)  for (__typeof(x.end()) itr = x.end() - 1; itr != x.begin() - 1; itr --)
+#define type(itr,x) __typeof(x.begin()) itr = x.begin()
+#define Inp freopen("a.inp","r",stdin);
+#define Out freopen("a.out","w",stdout);
+#define N 100
+#define ve vector
+typedef long long      ll;
+typedef pair<int, int> ii;
+typedef pair<ii, int>  iii;
+typedef vector<int>    vi;
+typedef vector<ii>     vii;
+typedef vector<iii>    viii;
+const   int            inf = 1e9;
+const   double         eps = 0;
+const   int            ms  = 0;
+const   int            md  = 0;
+
+// PlayGround
+int n,a[N],M,f[1000][100];
+int dp(int m,int id){
+    if(id == n) return (m<=M)?0:-1e8;
+    int &res = f[m][id];
+    if(res !=-1) return res;
+    res = max(dp(m,id+1),a[id]+dp(m+a[id],id+1));
+    return res;
+}
+int main(){
+    Inp
+    cin>>n>>M;
+    fill(f,-1);
+    rep(i,0,n){
+        cin>>a[i];
+    }
+    cout<<dp(0,0)<<endl;
+return 0;
+}
+//1 4 4 5 7 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
